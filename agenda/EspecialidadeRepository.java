d83334743a3865de9320dad38c4a24eb3707462d
Gabriel Wellington <124045962+GabrielWellington@users.noreply.github.com>
2025-11-27 14:06:15 -0300

    Delete agenda directory

--- agenda/EspecialidadeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.trabalho.agenda.repository;
-
-import com.trabalho.agenda.model.Especialidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {}
